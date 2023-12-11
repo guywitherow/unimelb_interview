0794a4f4700b04a8f1b4625699855d9ca88025d9
Guy Witherow <s3783428@student.rmit.edu.au>
2023-12-11 12:50:23 +1100

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
+import React, { useState, useCallback, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        'Fraunces': require('./assets/fonts/Fraunces.ttf'),
+      });
+      setFontsLoaded(true);
+      await SplashScreen.hideAsync();
+    };
+
+    loadFonts();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {fontsLoaded ? (
+        <>
+          <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </>
+      ) : null}
     </View>
   );
 }
@@ -13,8 +36,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000C38',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: 'Fraunces',
+    color: '#D2CEC8',
+  }
 });
